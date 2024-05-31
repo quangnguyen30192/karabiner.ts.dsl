641b5d19941199006c5e9440af92dcab1c7707b8
Quang Nguyen <quangnbnse90114@gmail.com>
2024-05-31 10:58:05 +0700

    refactor mouse #2

--- src/mouses.ts
+++ src/mouses.ts
@@ -1,110 +1,101 @@
-import { RuleBuilder, ToMouseKey, ifVar, map, rule } from "karabiner.ts";
+import {
+  FromKeyCode,
+  RuleBuilder,
+  ToEvent,
+  ToMouseKey,
+  ifVar,
+  map,
+  rule,
+  withCondition,
+} from "karabiner.ts";
 
 const WHEEL_SPEED = 50;
 const SLOWER_SPEED = 300;
 const NORMAl_SPEED = 718;
 const FASTER_SPEED = 2536;
-const MOUSE_MODE_KEY = "right_shift";
+const NAV_KEY = ["w", "s", "a", "d"];
 
 export function buildMouseKeys(): RuleBuilder {
   return rule("Control mouse by keys").manipulators([
-    map(MOUSE_MODE_KEY)
-      .toVar("mouse_mode", 1)
-      .toAfterKeyUp({
-        set_variable: { name: "mouse_mode", value: 0 },
-      }),
-
-    map("slash")
-      .toVar("mouse_faster_move_speed", 1)
-      .toAfterKeyUp({
-        set_variable: { name: "mouse_faster_move_speed", value: 0 },
-      })
-      .condition(ifMouseMode()),
-
     map("right_control")
-      .toVar("mouse_slower_move_speed", 1)
-      .toAfterKeyUp({
-        set_variable: { name: "mouse_slower_move_speed", value: 0 },
-      })
+      .toVar("mouse_mode")
+      .toAfterKeyUp(resetVar("mouse_mode")),
+
+    map("delete_forward")
+      .toVar("mouse_faster_move_speed")
+      .toAfterKeyUp(resetVar("mouse_faster_move_speed"))
       .condition(ifMouseMode()),
 
-    map("w")
-      .toMouseKey(moveUp(SLOWER_SPEED))
-      .condition(ifMoveSlower())
-      .description("mouse move up slower with w"),
-    map("w")
-      .toMouseKey(moveUp(FASTER_SPEED))
-      .condition(ifMoveFaster())
-      .description("mouse move up faster with w"),
-    map("w")
-      .toMouseKey(moveUp(NORMAl_SPEED))
-      .condition(ifMouseMode())
-      .description("mouse move up with w"),
+    map("right_shift")
+      .toVar("mouse_slower_move_speed")
+      .toAfterKeyUp(resetVar("mouse_slower_move_speed"))
+      .condition(ifMouseMode()),
 
-    map("s")
-      .toMouseKey(moveDown(SLOWER_SPEED))
-      .condition(ifMoveSlower())
-      .description("mouse move down slower with s"),
-    map("s")
-      .toMouseKey(moveDown(FASTER_SPEED))
-      .condition(ifMoveFaster())
-      .description("mouse move down faster with s"),
-    map("s")
-      .toMouseKey(moveDown(NORMAl_SPEED))
-      .condition(ifMouseMode())
-      .description("mouse move down with s"),
+    withCondition(ifMoveSlower())([
+      map(NAV_KEY.at(0) as FromKeyCode)
+        .toMouseKey(moveUp(SLOWER_SPEED))
+        .description("mouse move up slower"),
+      map(NAV_KEY.at(1) as FromKeyCode)
+        .toMouseKey(moveDown(SLOWER_SPEED))
+        .description("mouse move down slower"),
+      map(NAV_KEY.at(2) as FromKeyCode)
+        .toMouseKey(moveLeft(SLOWER_SPEED))
+        .description("mouse move left slower"),
+      map(NAV_KEY.at(3) as FromKeyCode)
+        .toMouseKey(moveRight(SLOWER_SPEED))
+        .description("mouse move right slower"),
+    ]),
 
-    map("a")
-      .toMouseKey(moveLeft(SLOWER_SPEED))
-      .condition(ifMoveSlower())
-      .description("mouse move left slower with a"),
-    map("a")
-      .toMouseKey(moveLeft(FASTER_SPEED))
-      .condition(ifMoveFaster())
-      .description("mouse move left faster with a"),
-    map("a")
-      .toMouseKey(moveLeft(NORMAl_SPEED))
-      .condition(ifMouseMode())
-      .description("mouse move left with a"),
+    withCondition(ifMoveFaster())([
+      map(NAV_KEY.at(0) as FromKeyCode)
+        .toMouseKey(moveUp(FASTER_SPEED))
+        .description("mouse move up faster"),
+      map(NAV_KEY.at(1) as FromKeyCode)
+        .toMouseKey(moveDown(FASTER_SPEED))
+        .description("mouse move down faste"),
+      map(NAV_KEY.at(2) as FromKeyCode)
+        .toMouseKey(moveLeft(FASTER_SPEED))
+        .description("mouse move left faster"),
+      map(NAV_KEY.at(3) as FromKeyCode)
+        .toMouseKey(moveRight(FASTER_SPEED))
+        .description("mouse move right faster"),
+    ]),
 
-    map("d")
-      .toMouseKey(moveRight(SLOWER_SPEED))
-      .condition(ifMoveSlower())
-      .description("mouse move right slower with d"),
-    map("d")
-      .toMouseKey(moveRight(FASTER_SPEED))
-      .condition(ifMoveFaster())
-      .description("mouse move right faster with d"),
-    map("d")
-      .toMouseKey(moveRight(NORMAl_SPEED))
-      .condition(ifMouseMode())
-      .description("mouse move right with d"),
+    withCondition(ifMouseMode())([
+      map(NAV_KEY.at(0) as FromKeyCode)
+        .toMouseKey(moveUp(NORMAl_SPEED))
+        .description("mouse move up"),
 
-    map("q")
-      .toPointingButton("button1")
-      .condition(ifMouseMode())
-      .description("Left click"),
+      map(NAV_KEY.at(1) as FromKeyCode)
+        .toMouseKey(moveDown(NORMAl_SPEED))
+        .description("mouse move down"),
 
-    map("q", "left_shift")
-      .toPointingButton("button1", "left_command")
-      .condition(ifMouseMode())
-      .description("Keymap for open a link in new tab"),
+      map(NAV_KEY.at(2) as FromKeyCode)
+        .toMouseKey(moveLeft(NORMAl_SPEED))
+        .description("mouse move left"),
 
-    map("e")
-      .toPointingButton("button2")
-      .condition(ifMouseMode())
-      .description("Right click"),
+      map(NAV_KEY.at(3) as FromKeyCode)
+        .toMouseKey(moveRight(NORMAl_SPEED))
+        .description("mouse move right"),
 
-    map("c").toMouseKey(scrollDown(WHEEL_SPEED)).condition(ifMouseMode()),
-    map("x").toMouseKey(scrollUp(WHEEL_SPEED)).condition(ifMouseMode()),
-    map("z").toMouseKey(scrollLeft(WHEEL_SPEED)).condition(ifMouseMode()),
-    map("v").toMouseKey(scrollRight(WHEEL_SPEED)).condition(ifMouseMode()),
+      map("q").toPointingButton("button1").description("Left click"),
+      map("q", "left_shift")
+        .toPointingButton("button1", "left_command")
+        .description("Keymap for open a link in new tab"),
+
+      map("e").toPointingButton("button2").description("Right click"),
+
+      map("c").toMouseKey(scrollDown(WHEEL_SPEED)),
+      map("x").toMouseKey(scrollUp(WHEEL_SPEED)),
+      map("z").toMouseKey(scrollLeft(WHEEL_SPEED)),
+      map("v").toMouseKey(scrollRight(WHEEL_SPEED)),
+    ]),
   ]);
 }
 
-const ifMoveFaster = () => ifVar("mouse_faster_move_speed")
-const ifMouseMode = () => ifVar("mouse_mode")
-const ifMoveSlower = () => ifVar("mouse_slower_move_speed")
+const ifMoveFaster = () => ifVar("mouse_faster_move_speed");
+const ifMouseMode = () => ifVar("mouse_mode");
+const ifMoveSlower = () => ifVar("mouse_slower_move_speed");
 
 function moveUp(y: number): ToMouseKey {
   return { speed_multiplier: 1.2, y: -y };
@@ -136,4 +127,10 @@ function scrollDown(speed: number): ToMouseKey {
 
 function scrollUp(speed: number): ToMouseKey {
   return { speed_multiplier: 1.2, vertical_wheel: speed };
+}
+
+function resetVar(name: string): ToEvent {
+  return {
+    set_variable: { name, value: 0 },
+  };
 }
